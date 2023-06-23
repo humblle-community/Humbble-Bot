0ca340017dcfd93589be2f2d2c16e2e5309f34d7
Hefri Anesti <hefrykun10@gmail.com>
2023-06-23 21:32:45 +0700

    app project

--- humblle-bot/app.py
+++ humblle-bot/app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api
 
 app = Flask(__name__, template_folder='templates')
 api = Api(app)
 
 @app.route('/')
-def main():
-    return {'message' : 'Hello'}
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4000)
